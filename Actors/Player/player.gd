8440ccd237a1631334ee7391936a4f5b51ff6a5a
BrunaLuisa <bruna2212br1@gmail.com>
2026-06-23 11:45:16 -0300

    fase 1 fimgit add .! pei pei pow pow

--- Actors/Player/player.gd
+++ Actors/Player/player.gd
@@ -4,7 +4,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 5.5
 
 @onready var health_component: HealthComponent = $HealthComponent
-@onready var visual_node: Node3D = $VisualVampira
+@onready var visual_node: Node3D = $VampiraNV
 @onready var area_ataque: Area3D = $AreaAtaque
 @onready var hud: CanvasLayer = $HUD
 
@@ -12,7 +12,7 @@ const JUMP_VELOCITY = 5.5
 @export var luz_lanterna: Node3D
 @export var velocidade_luz: float = 5.0 # Controla a rapidez com que a luz segue a Vânia
 
-
+@onready var anim_player: AnimationPlayer = $VampiraNV/AnimationPlayer
 
 var spawn_position: Vector3
 
@@ -77,10 +77,26 @@ func _physics_process(delta: float) -> void:
 		)
 
 	velocity.x = 0
+	
+	# --- NOVO: CONTROLE DE ANIMAÇÕES ---
+	# 1. Verificamos se a animação de ataque está tocando
+	var is_attacking = anim_player.current_animation == "animacoes/chutar" and anim_player.is_playing()
+	
+	# 2. Só mudamos a animação de movimento se ELA NÃO ESTIVER ATACANDO
+	if not is_attacking:
+		if not is_on_floor():
+			anim_player.play("animacoes/pular") # Troque pelo nome exato da sua animação de pulo
+		elif direction != Vector3.ZERO:
+			anim_player.play("animacoes/correr") # Troque pelo nome exato da sua animação de corrida
+		else:
+			anim_player.play("animacoes/idle") # Troque pelo nome exato da sua animação de ficar parada
+	# -----------------------------------
 
 	move_and_slide()
 
 func atacar() -> void:
+	# NOVO: Toca a animação de ataque assim que a função é chamada
+	anim_player.play("animacoes/chutar") # Troque pelo nome exato da sua animação de ataque
 	var corpos_acertados = area_ataque.get_overlapping_bodies()
 
 	for corpo in corpos_acertados:
